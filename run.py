8a518b50d672b0a70ff71bc0f248b8bced101b68
陈梓俊 <1301215701@qq.com>
2021-11-30 17:09:43 +0800

    feat: final

--- run.py
+++ run.py
@@ -26,23 +26,14 @@ def runApplication(filePath):
 
 if __name__ == '__main__':
     argc = len(sys.argv)
-    print(argc)
     if(argc == 1):
         print(f'Please input a file path to start running')
     else:
         filename = sys.argv[1]
-        print(filename)
+        print("constructing: "+filename)
         runApplication(filename)
     # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.crunchyroll.crmanga_1.png'
     # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.dropbox.android_2.png'
     # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.amazon.mShop.android.shopping_2.png'
     # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\codeadore.textgram_2.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.infonow.bofa_1.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.netflix.mediaclient_1.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.crunchyroll.crmanga_2.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.giphy.messenger_1.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.allfootball.news_2.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.reddit.frontpage_2.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.allfootball.news_3.png'
-    # filename = r'C:\Users\86134\Desktop\autotest_tool\ML_GUI_Prototype\screenshot\com.apalon.ringtones_1.png'
     # runApplication(filename)
